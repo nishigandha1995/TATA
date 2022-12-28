713a8cb66f5d9ea079257c7aa68428e9f4d73c08
nishi <nishi@gmail.com>
2022-12-29 03:05:57 +0530

    Hello

---
+++ src/main/java/com/example/TATA/TataApplication.java
@@ -0,0 +1,13 @@
+package com.example.TATA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TataApplication.class, args);
+	}
+
+}
