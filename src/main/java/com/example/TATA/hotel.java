f1c4898aa95f76d045d422b9eafc7730e02f264f
nishi <nishi@gmail.com>
2022-12-29 03:13:29 +0530

    hello

---
+++ src/main/java/com/example/TATA/hotel.java
@@ -0,0 +1,13 @@
+package com.example.TATA;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class hotel {
+    @GetMapping("/myhotel")
+    public String getData(){
+        return "Please book sss your  hotel ticket, Choose Your Fav Airlines Idigo";
+    }
+
+}
