91f81a38078ed1901ad3659b8813d5f5e7d3af46
nishi kutemate <nishik@gmail.com>
2023-01-05 07:10:38 +0530

    adding my first demo file

---
+++ src/main/java/com/example/TATA/demo.java
@@ -0,0 +1,13 @@
+package com.example.TATA;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class demo {
+    @GetMapping("/mydemo")
+    public String getData(){
+        return "Please book your  Flight ticket, Choose Your Fav Airlines Idigo";
+    }
+
+}
